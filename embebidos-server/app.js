3cf8fdd4517505a69d1c52df6512543a3d5993c8
loloide <lorenzo.dlc.arg@gmail.com>
2026-05-08 18:53:58 -0300

    deploy

--- embebidos-server/app.js
+++ embebidos-server/app.js
@@ -1,11 +1,24 @@
+require("dotenv").config()
 const express = require("express");
 const path = require("path");
 const admin = require("firebase-admin");
 
 const app = express();
 
-// Initialize Firebase Admin SDK
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  // The .replace() fix is critical here:
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURIComponent(process.env.CLIENT_EMAIL)}`,
+  universe_domain: "googleapis.com"
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
